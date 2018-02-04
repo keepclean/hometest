782fd018fbd317a29f0de910e9cc1cdfb2c27824
Rustam Iuldashev <rustam.iuldashev@protonmail.com>
2018-02-04 18:33:17 +0300

    Rewrited "ip_sanity_check" func: return ipaddress.Interface object instead custom dict

--- tests/test_find_lines.py
+++ tests/test_find_lines.py
@@ -24,4 +24,4 @@ class TestFindLines:
             find_lines(ip, lf)
 
         out, err = capfd.readouterr()
-        assert(err == '[Warning] The src_ip "31.184.238.128/33" is invalid; Skip line;')
+        assert(err == '[Warning] The src_ip "31.184.238.128/33" is invalid; Skip line;\n')
--- tests/test_ip_sanity_check.py
+++ tests/test_ip_sanity_check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import pytest
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -10,21 +11,17 @@ from weblog_helper import ip_sanity_check
 class TestIpSanityCheck:
 
     def test_invalid_ipv4_address(self):
-        assert(
-            ip_sanity_check(u'192.168.1.1000')['status'] is False
-        )
+        with pytest.raises(ValueError):
+            ip_sanity_check(u'192.168.1.1000')
 
     def test_invalid_ipv6_address(self):
-        assert(
-            ip_sanity_check(u'::g')['status'] is False
-        )
+        with pytest.raises(ValueError):
+            ip_sanity_check(u'::g')
 
     def test_invalid_ipv4_network(self):
-        assert(
-            ip_sanity_check(u'192.168.1.0/33')['status'] is False
-        )
+        with pytest.raises(ValueError):
+            ip_sanity_check(u'192.168.1.0/33')
 
     def test_invalid_ipv6_network(self):
-        assert(
-            ip_sanity_check(u'::1/129')['status'] is False
-        )
+        with pytest.raises(ValueError):
+            ip_sanity_check(u'::1/129')
